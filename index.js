1a095f6c958a52668a894db563ace90d709d7892
Harsh Raj <sdharshraj@gmail.com>
2016-11-22 15:03:02 +0530

    gif

--- index.js
+++ index.js
@@ -80,39 +80,10 @@ function sendTextMessage(sender, text) {
 
 function sendGenericMessage(sender) {
 	let messageData = {
-		attachment: {
-        type: "template",
+		      attachment: {
+        type: "image",
         payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: SERVER_URL + "/assets/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: SERVER_URL + "/assets/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
+          url: SERVER_URL + "/assets/instagram_logo.gif"
         }
       }
 	}
